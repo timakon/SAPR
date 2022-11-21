d4a434ef0d8303edcc0892c3bf29d38bc01e3844
timakon <1910tim@mail.ru>
2022-11-21 22:02:36 +0300

    refactor v.2.0

--- task5/task5.py
+++ task5/task5.py
@@ -1,59 +1,79 @@
-import numpy as np
-
-import json
 from io import StringIO
-
+import numpy as np
+import json
 
 SEQ_LEN = 10
 
-
-def make_row(visited: set, cur: int) -> np.array:
+def createRow(visited set, cur int) - np.array
     row = []
-    for i in range(SEQ_LEN):
+    for i in range(SEQ_LEN)
         row.append(1 if i+1 in visited else 0)
     return np.array(row)
 
-def make_matrix(data: list) -> np.array:
+def createMatrix(data list) - np.array
     visited = set()
     matrix = list()
 
-    for elem in data:
-        if type(elem) == str:
+    for elem in data
+        if type(elem) == str
             visited.add(int(elem))
-            row = make_row(visited=visited, cur=int(elem))
-            matrix.append({'num': int(elem), 'row': row})
-        else:
-            for subelem in elem:
+            row = createRow(visited=visited, cur=int(elem))
+            matrix.append({'num' int(elem), 'row' row})
+        else
+            for subelem in elem
                 visited.add(int(subelem))
-            for subelem in elem:
-                row = make_row(visited=visited, cur=int(subelem))
-                matrix.append({'num': int(subelem), 'row': row})
+            for subelem in elem
+                row = createRow(visited=visited, cur=int(subelem))
+                matrix.append({'num' int(subelem), 'row' row})
 
-    matrix.sort(key=(lambda x: x['num']))
+    matrix.sort(key=(lambda x x['num']))
     raw = [elem['row'] for elem in matrix]
 
     return np.array(raw)
 
+def findErr(json_path str) - list
+    data = json.loads(open(json_path).read())
 
-def get_json_data(json_path1, json_path2):
-    data1 = json.loads(open(json_path1).read())
-    data2 = json.loads(open(json_path2).read())
-    return find_controversies(make_matrix(data1), make_matrix(data2))
+    matrix1 = createMatrix(data['input1'])
+    matrix2 = createMatrix(data['input2'])
 
-
-def find_controversies(data1, data2):
-    matrix12 = data1 * data2
-    matrix12T = data1.T * data2.T
+    matrix12 = matrix1  matrix2
+    matrix12T = matrix1.T  matrix2.T
 
     criterion = np.logical_or(matrix12, matrix12T)
 
     answer = []
-    for i in range(criterion.shape[0]):
-        for j in range(i):
-            if not criterion[i][j]:
-                answer.append((['{}'.format(j+1), '{}'.format(i+1)]))
+    for i in range(criterion.shape[0])
+        for j in range(i)
+            if not criterion[i][j]
+                answer.append([j+1, i+1])
+    
+    flag = True
+    for row in data['output']
+        flag = ([int(row[0]), int(row[1])] in answer )
+        if not flag
+            print(fОшибочка {row})
+    if flag
+        print(Всё прошло успешно!)
 
     return answer
 
+def task(str1, str2) - list
+    str1 = eval(str1)
+    str2 = eval(str2)    
+    
+    matrix1 = createMatrix(str1)
+    matrix2 = createMatrix(str2)
 
-get_json_data("example.json", "example2.json")
+    matrix12 = matrix1  matrix2
+    matrix12T = matrix1.T  matrix2.T
+
+    criterion = np.logical_or(matrix12, matrix12T)
+
+    answer = []
+    for i in range(criterion.shape[0])
+        for j in range(i)
+            if not criterion[i][j]
+                answer.append([str(j+1), str(i+1)])
+                
+    return answer
